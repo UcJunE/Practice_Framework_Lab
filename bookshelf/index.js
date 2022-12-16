4b9d503fd2ad12d2ba20aefdd025af3b5d2cea55
UcJunE <ucjune90@gmail.com>
2022-12-16 08:51:34 +0000

    setup bookshelf

---
+++ bookshelf/index.js
@@ -0,0 +1,14 @@
+const Bookshelf = require("bookshelf");
+
+const knew = require("knex")({
+  client: "mysql",
+  connection: {
+    user: "foo",
+    password: "bar",
+    database: "poster_shop",
+  },
+});
+
+const bookshelf = require("bookshelf")(knex);
+
+module.exports = bookshelf;
--- routes/poster.js
+++ routes/poster.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router(); // #1 - Create a new express Router
 
 //  #2 Add a new route to the Express router
-router.get("/add", (req, res) => {
-  res.send("add new");
+router.get("/", (req, res) => {
+  res.send("get all poster");
 });
+
+router.get("/add",(req,res)=>{
+  res.send("add new poster")
+})
 
 module.exports = router; // #3 export out the router
