1aca5ebc12b1a6a28a0970a5a1607c97f1225596
UcJunE <ucjune90@gmail.com>
2022-12-19 10:10:40 +0000

    added column for category and foreign key

--- models/index.js
+++ models/index.js
@@ -2,6 +2,15 @@ const bookshelf = require("../bookshelf");
 
 const Poster = bookshelf.model("Poster", {
   tableName: "posters",
+  category() {
+    return this.belongTo("Category");
+  },
 });
 
-module.exports = { Poster };
+const Category = bookshelf.model("Category", {
+  tableName: "categories",
+  posters() {
+    return this.hasMany("Poster");
+  },
+});
+module.exports = { Poster, Category };
